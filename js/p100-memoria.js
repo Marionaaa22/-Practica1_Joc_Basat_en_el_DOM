5846d8abb788fe4643ac4de7f2aca0218b73625f
Marionaaa22 <mariona.claros@mataro.epiaedu.cat>
2026-04-16 11:39:08 +0200

    girar cartes

--- js/p100-memoria.js
+++ js/p100-memoria.js
@@ -90,7 +90,7 @@ function posicionarCartes() {
 
 function girarCarta() {
 
-    if ($(this).hasClass("carta-girada") || girades.length === 2) return;
+    if ($(this).hasClass("carta-girada")) return;
 
     $(this).addClass("carta-girada");
     girades.push($(this));
@@ -98,29 +98,6 @@ function girarCarta() {
     clics++;
     $("#contador").text(clics);
 
-    if (girades.length === 2) {
-        comprobarPareja();
-    }
-
-
 }
 
-function comprobarPareja() {
 
-    let c1 = girades[0].find(".davant").attr("class");
-    let c2 = girades[1].find(".davant").attr("class");
-
-    if (c1 === c2) {
-        setTimeout(() => {
-            girades[0].fadeOut();
-            girades[1].fadeOut();
-            girades = [];
-        }, 500);
-    } else {
-        setTimeout(() => {
-            girades[0].removeClass("carta-girada");
-            girades[1].removeClass("carta-girada");
-            girades = [];
-        }, 800);
-    }
-}
