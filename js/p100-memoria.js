d7e55a72727009759401a833b323a4a785211386
Sergi Farres <sergitofarres@gmail.com>
2026-04-28 11:23:31 +0200

    ImplementacioCodigCambiarCartes

--- js/p100-memoria.js
+++ js/p100-memoria.js
@@ -11,16 +11,41 @@ var temps = 0;
 var timer = null;
 var ultimaCarta = null;
 
+const DECK = 1;
+const POKEM = 2;
+const POKER = 3;
+
+var tipoCarta;
+var separacioH = 20, separacioV = 20;
+var deckWidth = 80, deckHeight = 120;
+var pokeWidth = 111, pokeHeight = 111;
+var pokerWidth = 79, pokerHeight = 124;
+
 $(function () {
 
-    iniciarJoc(2, 2);
+    iniciarJoc(2, 2,POKER);
 
 });
 
-function iniciarJoc(files, columnes) {
+function iniciarJoc(files, columnes, type) {
 
     nFiles = files;
     nColumnes = columnes;
+
+    if (type == DECK) {
+        ampladaCarta = deckWidth;
+        alcadaCarta = deckHeight;
+        tipoCarta = "deck";
+
+    } else if (type == POKEM) {
+        ampladaCarta = pokeWidth;
+        alcadaCarta = pokeHeight;
+        tipoCarta = "pokem";
+    }else if (type == POKER) {
+        ampladaCarta = pokerWidth;
+        alcadaCarta = pokerHeight;
+        tipoCarta = "poker";
+    }
 
     clics = 0;
     $("#contador").text(clics);
@@ -28,7 +53,7 @@ function iniciarJoc(files, columnes) {
     $("#contadorParelles").text(parellesContador);
     generarTauler();
     generarCartes();
-    posicionarCartes();
+    posicionarCartes(type);
 
     contarTemps(files, columnes, temps);
 
@@ -50,9 +75,6 @@ function generarTauler() {
             $("#tauler").append(carta);
         }
     }
-
-    ampladaCarta = $(".carta").width();
-    alcadaCarta = $(".carta").height();
 
     let ample = nColumnes * (ampladaCarta + separacio);
     let alt = nFiles * (alcadaCarta + separacio);
@@ -81,7 +103,7 @@ function generarCartes() {
     cartes = base.sort(() => Math.random() - 0.5);
 }
 
-function posicionarCartes() {
+function posicionarCartes(type) {
 
     let index = 0;
     $(".carta").each(function () {
@@ -89,11 +111,25 @@ function posicionarCartes() {
         let c = (index % nColumnes) + 1;
 
         $(this).css({
+            "width": ampladaCarta + "px",
+            "height": alcadaCarta + "px",
             "left": ((c - 1) * (ampladaCarta + separacio) + separacio) + "px",
             "top": ((f - 1) * (alcadaCarta + separacio) + separacio) + "px"
         });
 
-        $(this).find(".davant").addClass(cartes[index]);
+        $(this).find(".davant").addClass(tipoCarta+cartes[index]);
+        if (type == DECK) {
+            $(this).find(".davant").removeClass("deck pokemon poker").addClass("deck");
+            $(this).find(".darrera").removeClass("deck pokemon poker").addClass("deck");
+        }
+        else if (type == POKEM) {
+            $(this).find(".davant").removeClass("deck pokemon poker").addClass("pokemon");
+            $(this).find(".darrera").removeClass("deck pokemon poker").addClass("pokemon");
+        }
+        else if (type == POKER) {
+            $(this).find(".davant").removeClass("deck pokemon poker").addClass("poker");
+            $(this).find(".darrera").removeClass("deck pokemon poker").addClass("poker");
+        }
         index++;
     });
 }
